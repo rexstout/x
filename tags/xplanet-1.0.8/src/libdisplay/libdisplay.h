830d5f09174d056920ecb1f92df38a4b5cebe0c8
hari <hari@63acc863-b1ee-4aef-a14a-77ae96d11755>
2006-03-26 04:40:49 +0000

    Mistakenly labeled 1.1.0 as 1.0.8

--- tags/xplanet-1.0.8/src/libdisplay/libdisplay.h
+++
@@ -1,8 +0,0 @@
-#ifndef LIBDISPLAY_H
-#define LIBDISPLAY_H
-
-#include "DisplayBase.h"
-
-extern DisplayBase *getDisplay(const int times_run);
-
-#endif
--- tags/xplanet-1.0.8/src/parse.h
+++
@@ -1,8 +0,0 @@
-#ifndef PARSE_H
-#define PARSE_H
-
-extern bool isDelimiter(char c);
-extern bool isEndOfLine(char c);
-extern int parse(int &i, const char *line, char *&returnstring);
-
-#endif
